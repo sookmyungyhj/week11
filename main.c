e9519dd6ca77275d72f756f697b34a652917dd3a
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-11-12 15:40:50 +0900

    3

--- main.c
+++ main.c
@@ -5,16 +5,14 @@
 
 int main(void)
 {
-	int i = 10;
-	int *ptr = &i;
-
-	printf("i = %d, pi : %p\n", i, ptr);
-
-	(*ptr)++;
-	printf("i = %d, pi : %p\n", i, ptr);
-
-	*ptr++;
-	printf("i = %d, pi : %p\n", i, ptr);
+	int i = 100;
+	int *p = &i;
+	int **q = &p;
+	
+	*p = 200;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
+	**q = 300;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q); 
 
 	return 0;
 }
